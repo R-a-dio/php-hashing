e5568673421321e33140054692f2e17ea236516a
David Collinson <hiroto@hiroto.eu>
2014-02-03 20:17:50 +0100

    Force Markdown to render newlines as newlines

--- Michelf/Markdown.php
+++ Michelf/Markdown.php
@@ -499,7 +499,7 @@ class Markdown implements MarkdownInterface {
 	
 	protected function doHardBreaks($text) {
 		# Do hard breaks:
-		return preg_replace_callback('/ {1,}\n/', 
+		return preg_replace_callback('/\n/', 
 			array(&$this, '_doHardBreaks_callback'), $text);
 	}
 	protected function _doHardBreaks_callback($matches) {
